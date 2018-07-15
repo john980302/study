277fbd14de96db65200e726c6c417141eee1e4a1
DESKTOP-33E0BGE\최우진 <john1920@naver.com>
2018-07-15 15:34:47 +0900

    백준 2577 수정후

--- C code/Baekjoon/Baekjoon 2577.c
+++ C code/Baekjoon/Baekjoon 2577.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
 	int a, b, c,res;
+	char r[10];
+	int ir[10] = { 0 };
+
 	scanf("%d", &a);
-	fflush(stdin);
 	scanf("%d", &b);
-	fflush;
 	scanf("%d", &c);
+	
 	res = a * b * c;
+	sprintf(r, "%d", res);
 
+	for (int i = 0; i < strlen(r); i++) {
+		ir[r[i] - '0']++;
+	}
 
+	for (int i = 0; i < 10; i++)
+		printf("%d\n", ir[i]);
 
 	return 0;
 }
